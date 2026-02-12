e78eadfdb05676954386019beb1c542cfb5bd25b
hhector7722 <hhector7722@gmail.com>
2026-02-12 15:28:30 +0100

    fix: remove unused BottomNavAdmin import to resolve build error

--- src/components/BottomNavWrapper.tsx
+++ src/components/BottomNavWrapper.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import BottomNavAdmin from './BottomNavAdmin';
 import BottomNavStaff from './BottomNavStaff';
 
 export default function BottomNavWrapper() {
