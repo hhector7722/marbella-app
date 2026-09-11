8da3a088cee29151b6f8670d395937cf3271487a
Hector <206630891+hhector7722@users.noreply.github.com>
2026-09-12 01:23:59 +0200

    wf

--- src/components/dashboards/MasterLastClosingWidget.tsx
+++ src/components/dashboards/MasterLastClosingWidget.tsx
@@ -245,7 +245,7 @@ export function MasterLastClosingWidget() {
                     </span>
                 </div>
             </div>
-            <div className="flex min-h-0 flex-1 flex-col justify-start px-2 pb-3.5 pt-3 lg:px-3 lg:pb-4 lg:pt-1">
+            <div className="flex min-h-0 flex-1 flex-col justify-start px-2 pb-3.5 pt-3 lg:px-3 lg:pb-3 lg:pt-0">
                 {loading ? (
                     <div
                         className="flex flex-1 items-center justify-center"
