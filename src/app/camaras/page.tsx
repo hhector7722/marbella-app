995b7b8dbc482a48cc051c958f08d3921298b58f
hhector7722 <hhector7722@gmail.com>
2026-09-15 19:14:55 +0200

    Track viewers on camera page

--- src/app/camaras/page.tsx
+++ src/app/camaras/page.tsx
@@ -5,6 +5,7 @@ import { canAccessCameras } from '@/lib/cameras/access';
 import { isMasterDashboardUser } from '@/lib/master-dashboard';
 import CameraLive from './CameraLive';
 import CameraRecordsButton from './CameraRecordsButton';
+import CameraViewerPresence from './CameraViewerPresence';
 
 export default async function CamerasPage() {
   const supabase = await createClient();
@@ -19,6 +20,7 @@ export default async function CamerasPage() {
 
   return (
     <div className="relative">
+      <CameraViewerPresence />
       {canSeeRecords ? (
         <div className="absolute right-4 top-3 z-30">
           <CameraRecordsButton />
